76af8ce60cf8cb80405835c963f7d7d83e901764
Gorazd <fi.gorazd@gmail.com>
2025-01-01 21:06:10 +0100

    add home page and navbar

---
+++ lib/screens/rewards_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RewardsScreen extends StatelessWidget {
+  const RewardsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Rewards')),
+      body: const Center(child: Text('Rewards Page')),
+    );
+  }
+}
