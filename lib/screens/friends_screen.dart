ae64bfc00cd81a2cf0f096a8c9aa12c0b56771c2
emsNikac <nikac.nikola@gmail.com>
2025-01-25 19:49:55 +0100

    Added Add Friend mock screen

--- lib/screens/friends_screen.dart
+++ lib/screens/friends_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/user.dart';
+import 'add_friends_screen.dart';
 
 class FriendsScreen extends StatelessWidget {
   final User user;
@@ -22,6 +23,42 @@ class FriendsScreen extends StatelessWidget {
             subtitle: Text(friend.bio),
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddFriendsScreen(user: user), // Updated screen name
+            ),
+          );
+        },
+        backgroundColor: Colors.orange,
+        icon: const Icon(Icons.add, color: Colors.white),
+        label: const Text(
+          "Add Friends Screen", // Updated label
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
+
+class AddFriendsMockScreen extends StatelessWidget {
+  const AddFriendsMockScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add Friends"),
+        backgroundColor: Colors.orange,
+      ),
+      body: const Center(
+        child: Text(
+          "This is a mock Add Friends screen.",
+          style: TextStyle(fontSize: 18, color: Colors.grey),
+        ),
       ),
     );
   }
